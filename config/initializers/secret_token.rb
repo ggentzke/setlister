cad2df9ba94e0f895adda2bf5e24798b0144b191
Glenn Gentzke <glenn.gentzke@gmail.com>
2012-11-17 13:33:22 -0300

    Initial commit and scaffold of song model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Setlister::Application.config.secret_token = '62bbe7b9c23af108930182dcfa75633449ca2ee980fce8a14f3d6acadfb419cad471952a469fceec67fd4007262e11c724259d5cffe0a23b35a9d7c5d69d4c49'
