1f2e743ddaf824592dfe541a565bfe74641e77ca
Glenn Gentzke <glenn.gentzke@gmail.com>
2012-11-24 14:51:08 -0300

    Adds non int case for music duration helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
   
   def music_duration total_seconds
+    return 'n/a' unless total_seconds.is_a? Fixnum
     duration_formatter = total_seconds >= 3600 ? '%H:%M:%S' : '%M:%S'
     Time.at(total_seconds).utc.strftime(duration_formatter)
   end
