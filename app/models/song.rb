b76e3b3f54323ef1b40da7f7aafb35dbe738f8e9
Glenn Gentzke <glenn.gentzke@gmail.com>
2013-03-18 23:43:43 -0400

    validations?  this commit has gotten stale

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,13 +5,31 @@ class Song < ActiveRecord::Base
   default_scope order('title ASC')
 
   validates_presence_of :title
+  validate :valid_duration
+  # validates :duration, numericality: { greater_than_or_equal_to: 0 }
   
-  after_initialize :set_default_values
+  # before_save :examine_duration
   
   private
   
-    def set_default_values
-      self.duration ||= 0
+    def examine_duration
+      if self.duration.present?
+        if self.duration.is_a? Fixnum
+          self.duration = 0 if self.duration < 0
+        elsif self.duration.is_a?(Hash)
+          temp = 0
+          temp += self.duration[:min]*60 if self.duration.has_key?(:min)
+          temp += self.duration[:sec] if self.duration.has_key?(:sec)
+          self.duration = temp
+        else
+          self.duration = 0
+        end
+      end
     end
-  
+   
+    def valid_duration
+      if duration.is_a? Hash
+        duration = 123
+      end
+    end
 end
